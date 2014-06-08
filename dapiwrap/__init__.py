023a796d77f6d02b2f76e4f56c1d6a81481292e8
Alex C. <trebek604@gmail.com>
2014-06-07 17:53:37 -0700

    Missed a couple more import statements.
    Like a dummy, I missed a couple more botched import statements. I am
    pretty damn sure I got them all this time.

--- dapiwrap/__init__.py
+++ dapiwrap/__init__.py
@@ -1,6 +1,6 @@
 from dapiwrap import DAPIWrap
-from dawglobs import *
-from dawtools import (
+from dapiwconst import *
+from dapiwtools import (
     Downwad, 
     print_wad_info,
     random_wad,
